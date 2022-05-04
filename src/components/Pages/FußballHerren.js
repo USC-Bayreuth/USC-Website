b019b51b54357f1824f85d16fdec7ea43c2cf1e8
StefanAmEnde <stefan.amende2000@gmail.com>
2022-05-04 14:13:01 +0200

    fupa

--- src/components/Pages/FußballHerren.js
+++ src/components/Pages/FußballHerren.js
@@ -31,7 +31,34 @@ class FußballHerren extends Component{
                 <h1>Unsere Abteilung</h1>
                 <p>Wir, die Herrenfußball-Abteilung des USC Bayreuth, heißen dich herzlich auf unserer Seite willkommen. Als Abteilung, aus welcher der USC entsprungen ist, bieten wir dir die Möglichkeit dich als Spieler, Funktionär oder in verschiedenen Bereichen rund um den Verein zu engagieren. Nach einigen Jahren in der Kreisklasse als SG Uni-Post SV Bayreuth gelang uns in der Saison 2015/16 und damit in der ersten Saison nach Gründung des USC der Aufstieg in die Kreisliga. Mit zwei überzeugenden Siegen auf großer Bühne konnten wir uns in den Relegationsspielen durchsetzen. Nachdem der erneute Aufstieg 16/17 denkbar knapp verpasst wurde, spielen wir aktuell in der Kreisliga Bayreuth/Kulmbach. Im Sommersemester haben wir das Privileg auf den hervorragenden Sportanlagen der Uni Bayreuth zu trainieren. Im Wintersemester, wenn die Uniplätze gesperrt sind, trainieren wir auf dem städtischen Kunstrasenplatz am Hans-Walter-Wild-Stadion, wo auch unsere Heimspiele ausgetragen werden, oder in der Turnhalle des Sportinstituts. Durch die hohe Fluktuation an Spielern sind wir immer auf der Suche nach neuem Personal. Wir freuen uns auf alle, die den Fußball genauso lieben wie wir. Bei Interesse vereinbare doch einfach ein Probetraining. Dafür kannst du entweder eine Facebook Nachricht senden oder eine Mail an <a className='mail-to' href='mailto:herrenfussball@uni-bayreuth.de'>herrenfussball@uni-bayreuth.de</a>.</p>
                 <h1>Tabelle und Spielplan</h1>
-                <script type="text/javascript" src="https://www.fupa.net/fupa/widget/fupa_widget.js" defer="defer"></script><div className="fupa_widget" data-widget='{"type":"team_widget","type_values":["tabelle"],"value":"961238","selected_tab":1}'><div> lade Widget... </div><a href="https://www.fupa.net/team/usc-bayreuth-m1-2021-22" className="widget_link" target="_blank" rel="noreferrer" title="USC Bayreuth auf FuPa" alt="USC Bayreuth auf FuPa" >USC Bayreuth auf FuPa</a></div>                    
+                <Container fluid className='my-container'>
+                    {this.state.mobileMode &&
+                        <>
+                            <Row>
+                                <Col>
+                                    <iframe src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62726b5448f17&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <iframe src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62722a97357d5&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    {!this.state.mobileMode &&
+                        <>
+                        <Row>
+                            <Col>
+                                <iframe src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62726b5448f17&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                            </Col>
+                            <Col>
+                                <iframe src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62722a97357d5&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                            </Col>
+                        </Row>
+                        </>
+                    }
+                </Container>
                 <h1>Aktueller Kader</h1>
                 <img alt='fußball-herren' src='/img/Fußball Herren/Mannschaftsfoto Herren.jpg' className='mannschaftsfoto'/>
                 <h2>Trainer</h2>
