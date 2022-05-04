6e7720f983dc2b020b1b54eafd97038dc1e43459
StefanAmEnde <stefan.amende2000@gmail.com>
2022-05-04 14:36:22 +0200

    fupa damen

--- src/components/Pages/FußballDamen.js
+++ src/components/Pages/FußballDamen.js
@@ -30,6 +30,34 @@ class FußballDamen extends Component{
                 <h1>Unsere Abteilung</h1>
                 <p>Die Frauenfußballabteilung des USC Bayreuth bietet den Studentinnen die Möglichkeit, ihre sportlichen Ziele zu erreichen, ihre Soft-Skills zu stärken und ihr Netzwerk zu erweitern. Zwei Trainingseinheiten während der Saison, ein Spiel am Wochenende und sowohl die Winter- als auch die Sommervorbereitung stellen einen perfekten Ausgleich für den oft trägen Unialltag. Nach dem Aufstieg in der Premierensaison gehen wir 18/19 in der Kreisliga an den Start. Du möchtest dich darüber hinaus aktiv mit einbringen? 4 Ressorts, darunter Organisation, Media, Sponsoring oder Events bieten dir die Möglichkeit dazu. Kommilitonen. Freunde. Teamkollegen. Bei mittlerweile über 30 Spielerinnen fällt der Kontakt nicht schwer. Haben wir dein Interesse geweckt? Dann komm vorbei – Wir freuen uns auf neue Gesichter! Melde dich einfach bei Facebook oder per Mail an <a className='mail-to' href='mailto:damenfussball@usc-bayreuth.de'>damenfussball@usc-bayreuth.de</a>.</p>
                 <h1>Tabelle und Spielplan</h1>
+                <Container fluid className='my-container'>
+                    {this.state.mobileMode &&
+                        <>
+                            <Row>
+                                <Col>
+                                    <iframe title='tabelle' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961899&div_id=widget_62726ef772c35&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <iframe title='spielplan' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961899&div_id=widget_62726f2f2fd90&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                                </Col>
+                            </Row>
+                        </>
+                    }
+                    {!this.state.mobileMode &&
+                        <>
+                        <Row>
+                            <Col>
+                                <iframe title='tabelle' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961899&div_id=widget_62726ef772c35&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                            </Col>
+                            <Col>
+                                <iframe title='spielplan' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961899&div_id=widget_62726f2f2fd90&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                            </Col>
+                        </Row>
+                        </>
+                    }
+                </Container>
                 <h1>Aktueller Kader</h1>
                 <img alt='fußball-damen' src='/img/Fußball Damen/Mannschaftsfoto Damen.jpg' className='mannschaftsfoto'/>
                 <h2>Trainer</h2>
