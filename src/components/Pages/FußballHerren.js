707fe49d21eca2951ed72cc4f7a8b81ad77d6919
StefanAmEnde <stefan.amende2000@gmail.com>
2022-05-29 12:18:15 +0200

    animation fix

--- src/components/Pages/FußballHerren.js
+++ src/components/Pages/FußballHerren.js
@@ -29,49 +29,37 @@ class FußballHerren extends Component{
             <>
                 <h1>Fußball Herren</h1>
                 <Slider tag='Fußball Herren'/>
-                {this.state.mobileMode &&
-                    <>
-                        <h1>Unsere Abteilung</h1>
-                        <p>Wir, die Herrenfußball-Abteilung des USC Bayreuth, heißen dich herzlich auf unserer Seite willkommen. Als Abteilung, aus welcher der USC entsprungen ist, bieten wir dir die Möglichkeit dich als Spieler, Funktionär oder in verschiedenen Bereichen rund um den Verein zu engagieren. Nach einigen Jahren in der Kreisklasse als SG Uni-Post SV Bayreuth gelang uns in der Saison 2015/16 und damit in der ersten Saison nach Gründung des USC der Aufstieg in die Kreisliga. Mit zwei überzeugenden Siegen auf großer Bühne konnten wir uns in den Relegationsspielen durchsetzen. Nachdem der erneute Aufstieg 16/17 denkbar knapp verpasst wurde, spielen wir aktuell in der Kreisliga Bayreuth/Kulmbach. Im Sommersemester haben wir das Privileg auf den hervorragenden Sportanlagen der Uni Bayreuth zu trainieren. Im Wintersemester, wenn die Uniplätze gesperrt sind, trainieren wir auf dem städtischen Kunstrasenplatz am Hans-Walter-Wild-Stadion, wo auch unsere Heimspiele ausgetragen werden, oder in der Turnhalle des Sportinstituts. Durch die hohe Fluktuation an Spielern sind wir immer auf der Suche nach neuem Personal. Wir freuen uns auf alle, die den Fußball genauso lieben wie wir. Bei Interesse vereinbare doch einfach ein Probetraining. Dafür kannst du entweder eine Facebook Nachricht senden oder eine Mail an <a className='mail-to' href='mailto:herrenfussball@uni-bayreuth.de'>herrenfussball@uni-bayreuth.de</a>.</p>                
-                    </>
-                }
-                {!this.state.mobileMode &&
-                    <AnimationOnScroll animateIn={this.props.animation} animateOnce>
-                        <h1>Unsere Abteilung</h1>
-                        <p>Wir, die Herrenfußball-Abteilung des USC Bayreuth, heißen dich herzlich auf unserer Seite willkommen. Als Abteilung, aus welcher der USC entsprungen ist, bieten wir dir die Möglichkeit dich als Spieler, Funktionär oder in verschiedenen Bereichen rund um den Verein zu engagieren. Nach einigen Jahren in der Kreisklasse als SG Uni-Post SV Bayreuth gelang uns in der Saison 2015/16 und damit in der ersten Saison nach Gründung des USC der Aufstieg in die Kreisliga. Mit zwei überzeugenden Siegen auf großer Bühne konnten wir uns in den Relegationsspielen durchsetzen. Nachdem der erneute Aufstieg 16/17 denkbar knapp verpasst wurde, spielen wir aktuell in der Kreisliga Bayreuth/Kulmbach. Im Sommersemester haben wir das Privileg auf den hervorragenden Sportanlagen der Uni Bayreuth zu trainieren. Im Wintersemester, wenn die Uniplätze gesperrt sind, trainieren wir auf dem städtischen Kunstrasenplatz am Hans-Walter-Wild-Stadion, wo auch unsere Heimspiele ausgetragen werden, oder in der Turnhalle des Sportinstituts. Durch die hohe Fluktuation an Spielern sind wir immer auf der Suche nach neuem Personal. Wir freuen uns auf alle, die den Fußball genauso lieben wie wir. Bei Interesse vereinbare doch einfach ein Probetraining. Dafür kannst du entweder eine Facebook Nachricht senden oder eine Mail an <a className='mail-to' href='mailto:herrenfussball@uni-bayreuth.de'>herrenfussball@uni-bayreuth.de</a>.</p>
-                    </AnimationOnScroll>
-                }
-                <AnimationOnScroll animateIn={this.props.animation} animateOnce>
-                    <h1>Tabelle und Spielplan</h1>
-                    <Container fluid className='my-container'>
-                        {this.state.mobileMode &&
-                            <>
-                                <Row>
-                                    <Col>
-                                        <iframe title='tabelle' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62726b5448f17&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
-                                    </Col>
-                                </Row>
-                                <Row>
-                                    <Col>
-                                        <iframe title='spielplan' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62722a97357d5&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
-                                    </Col>
-                                </Row>
-                            </>
-                        }
-                        {!this.state.mobileMode &&
-                            <>
+                <h1>Unsere Abteilung</h1>
+                <p>Wir, die Herrenfußball-Abteilung des USC Bayreuth, heißen dich herzlich auf unserer Seite willkommen. Als Abteilung, aus welcher der USC entsprungen ist, bieten wir dir die Möglichkeit dich als Spieler, Funktionär oder in verschiedenen Bereichen rund um den Verein zu engagieren. Nach einigen Jahren in der Kreisklasse als SG Uni-Post SV Bayreuth gelang uns in der Saison 2015/16 und damit in der ersten Saison nach Gründung des USC der Aufstieg in die Kreisliga. Mit zwei überzeugenden Siegen auf großer Bühne konnten wir uns in den Relegationsspielen durchsetzen. Nachdem der erneute Aufstieg 16/17 denkbar knapp verpasst wurde, spielen wir aktuell in der Kreisliga Bayreuth/Kulmbach. Im Sommersemester haben wir das Privileg auf den hervorragenden Sportanlagen der Uni Bayreuth zu trainieren. Im Wintersemester, wenn die Uniplätze gesperrt sind, trainieren wir auf dem städtischen Kunstrasenplatz am Hans-Walter-Wild-Stadion, wo auch unsere Heimspiele ausgetragen werden, oder in der Turnhalle des Sportinstituts. Durch die hohe Fluktuation an Spielern sind wir immer auf der Suche nach neuem Personal. Wir freuen uns auf alle, die den Fußball genauso lieben wie wir. Bei Interesse vereinbare doch einfach ein Probetraining. Dafür kannst du entweder eine Facebook Nachricht senden oder eine Mail an <a className='mail-to' href='mailto:herrenfussball@uni-bayreuth.de'>herrenfussball@uni-bayreuth.de</a>.</p>                
+                <h1>Tabelle und Spielplan</h1>
+                <Container fluid className='my-container'>
+                    {this.state.mobileMode &&
+                        <>
                             <Row>
                                 <Col>
                                     <iframe title='tabelle' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62726b5448f17&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
                                 </Col>
+                            </Row>
+                            <Row>
                                 <Col>
                                     <iframe title='spielplan' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62722a97357d5&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
                                 </Col>
                             </Row>
-                            </>
-                        }
-                    </Container>
-                </AnimationOnScroll>
+                        </>
+                    }
+                    {!this.state.mobileMode &&
+                        <>
+                        <Row>
+                            <Col>
+                                <iframe title='tabelle' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62726b5448f17&start=tabelle&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                            </Col>
+                            <Col>
+                                <iframe title='spielplan' src='https://www.fupa.net/fupa/widget.php?p=iframe_widget&typ=team&value_id=961238&div_id=widget_62722a97357d5&start=spielplan&header=0&navi=0' frameBorder='0' className='fupa-widget'/>
+                            </Col>
+                        </Row>
+                        </>
+                    }
+                </Container>
                 <AnimationOnScroll animateIn={this.props.animation} animateOnce>
                     <h1>Aktueller Kader</h1>
                     <img alt='fußball-herren' src='/img/Fußball Herren/Mannschaftsfoto Herren.jpg' className='mannschaftsfoto'/>
