b6f8836cf160650fd634f00e5471286d3fb25786
StefanAmEnde <stefan.amende2000@gmail.com>
2022-07-04 12:48:18 +0200

    news und sponsoren

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -32,7 +32,7 @@ class Home extends Component{
                 <p>Als Studentenverein gewährt der USC ein familiäres, freundliches und respektvolles Umfeld. Es bietet sich besonders für Neulinge an der Universität und in Bayreuth die Möglichkeit an, durch den Sport Kontakte zu knüpfen und Freunde zu finden. Im Einklang dazu stehen auch die Werte und die Philosophie des USC: Wir sind ein Verein für alle Menschen, ungeachtet der sozialen oder kulturellen Herkunft oder des Geschlechts.</p>
                 <h1>Unser Premiumpartner</h1>
                 <a href='https://www.rehau.com/de-de' rel="noreferrer" target="_blank">
-                    <img src='/img/Home/Rehau-logo.png' alt='rehau' className='partner'/>
+                    <img src='/img/Sponsoren/Rehau-logo.png' alt='rehau' className='partner'/>
                 </a>
                 <h1>Social Media</h1>
                 <Container className='my-container' fluid>
@@ -49,7 +49,7 @@ class Home extends Component{
                 </Container>
                 <h1>Onlineshop</h1>
                     <a href='https://usc-bayreuth.fan12.de/' target='_blank' rel='noreferrer'>
-                        <img src='/img/Home/Fan12_Logo_RGB.png' className='partner' alt='fan-12-logo'/>
+                        <img src='/img/Sponsoren/Fan12_Logo_RGB.png' className='partner' alt='fan-12-logo'/>
                     </a>
             </>
         )
--- src/components/Pages/UnsereSponsoren.js
+++ src/components/Pages/UnsereSponsoren.js
@@ -4,13 +4,22 @@ class UnsereSponsoren extends Component{
     render(){
         return(
             <>
-                <h1>Unser Premiumpartner</h1>
-                    <a href='https://www.rehau.com/de-de' target="_blank" rel="noreferrer"><img src='/img/Home/Rehau-logo.png' alt='rehau' className='partner'/></a>
-                <h1>Classicpartner</h1>
+                <h1>Premiumpartner</h1>
+                <a href='https://www.rehau.com/de-de' target="_blank" rel="noreferrer"><img src='/img/Sponsoren/Rehau-logo.png' alt='rehau' className='partner'/></a>
                 <h1>Partner</h1>
+                <h2>Fußball Herren</h2>
+                <a href='https://purehardseltzer.de/' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/PURE-Logo.png' alt='pure' className='partner'/></a>
+                <h2>Fußball Damen</h2>
+                <a href='https://www.hausarztpraxis-bayreuth.de/' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/moos_willner.png' alt='moos_willner' className='partner'/></a>
+                <div/>
+                <a href='https://www.allfinanz-dvag.de/sebastian.krause/index.html' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/Sebastian_Krause.jpg' alt='dvag' className='partner'/></a>
+                <h2>Tennis</h2>
+                <a href='https://www.raumedic.com/' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/raumedic-Logo-weiß.png' alt='raumedic' className='partner'/></a>
+                <h2>Roudnet</h2>
+                <a href='https://www.naupaka.de/' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/naupaka.png' alt='naupaka' className='partner'/></a>
                 <h1>Teamausrüster</h1>
                 <a href='https://usc-bayreuth.fan12.de/' target='_blank' rel='noreferrer'>
-                    <img src='/img/Home/Fan12_Logo_RGB.png' className='partner' alt='fan-12-logo'/>
+                    <img src='/img/Sponsoren/Fan12_Logo_RGB.png' className='partner' alt='fan-12-logo'/>
                 </a>
             </>
         )
