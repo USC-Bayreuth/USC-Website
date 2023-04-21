562dffbfc0bb501778df894f9893d0f76576033d
StefanAmEnde <stefan.amende2000@gmail.com>
2023-04-21 20:32:49 +0200

    Update Volleyball.js

--- src/components/Pages/Volleyball.js
+++ src/components/Pages/Volleyball.js
@@ -27,7 +27,7 @@ class Volleyball extends Component{
                 <Slider tag='Volleyball'/>
                 <h1>Unsere Abteilung</h1>
                 <p>Der USC stellt sich breiter auf und bietet ab diesem Sommersemester die Möglichkeit zum Hallen-Volleyball an. Bei Lust und Interesse komm gerne einfach zum Training vorbei. Ziel ist es, in der kommenden Saison mit einer Wettkampfmannschaft an den Start zu gehen. Jede/r ist willkommen, natürlich auch Anfänger!</p>
-                <p>Melde dich bei Fragen jederzeit über unsere Social Media Kanäle (<a className='link' href='https://www.facebook.com/uscbayreuth/' target="_blank" rel="noreferrer">Facebook</a>, <a className='link' href='https://www.instagram.com/usc_bayreuth/' target="_blank" rel="noreferrer">Instagram</a>) oder bei <a className='link' href='mailto:info@usc-bayreuth.de'>info@usc-bayreuth.de</a>.</p>
+                <p>Melde dich bei Fragen jederzeit bei unserem Abteilungsleiter Duc (<a className='link' href='mailto:volleyball@usc-bayreuth.de'>volleyball@usc-bayreuth.de</a>) oder über unsere Social Media Kanäle (<a className='link' href='https://www.facebook.com/uscbayreuth/' target="_blank" rel="noreferrer">Facebook</a>, <a className='link' href='https://www.instagram.com/usc_bayreuth/' target="_blank" rel="noreferrer">Instagram</a>).</p>
                 <h2>Trainingszeiten</h2>
                 <p>Montag: 18:30 - 20:00 (Halle 2, Sportinstitut)</p>
                 <h1>Team</h1>
