350f79e8618990d05b85dde433620f10fe5c455b
Erik <reinscherik1@gmail.com>
2025-04-25 01:33:54 +0200

    Roundnet Update

--- src/components/Pages/Roundnet.js
+++ src/components/Pages/Roundnet.js
@@ -1,6 +1,7 @@
 import {Component} from 'react'
 import {Container, Row, Col} from 'react-bootstrap'
 
+
 class Roundnet extends Component{
     state={mobileMode: window.innerWidth<960}
 
@@ -19,31 +20,63 @@ class Roundnet extends Component{
             newState.mobileMode=true
         this.setState(newState)
     }
+
+
     render(){
+        let kader=require('../../json/Roundnet_Kader.json')
         return(
             <>
                 <h1>Roundnet</h1>
-                <p>Wir bekommen Zuwachs durch die neu gegründete Abteilung "Roundnet" (Spikeball) und sind somit der erste Roundnet Verein Bayreuths!</p>
-                <p>Du zockst gerne Roundnet und hast Bock an strukturiertem Training sowie Teil der Community zu sein oder ab und an auf ein Turnier zu fahren? Dann komm vorbei und schnupper bei uns rein.</p>          
-                <p>Bei Fragen meldet euch einfach bei <a className='link' href='mailto:roundnet.bayreuth@gmail.com'>roundnet.bayreuth@gmail.com</a> oder bei <a className='link' target="_blank" rel="noreferrer" href='https://www.instagram.com/bayreuth.baboons/'>@bayreuth.baboons</a>.</p>
-                <h2>Trainingszeiten (Winter)</h2>
-                <p>Dienstag 20:00 - 22:00 (<a className='link' href='https://maps.app.goo.gl/DM2CvnAiZUbCKW2n8' target='_blank' rel='noreferrer'>Halle Sportinstitut</a>)</p>
-                <p style={{marginTop: 15}}>Mittwoch 15:30 - 17:00 (<a className='link' href='https://maps.app.goo.gl/DM2CvnAiZUbCKW2n8' target='_blank' rel='noreferrer'>Halle Sportinstitut</a>)</p>
-                <p style={{marginTop: 15}}>Samstag 10:00 - 12:00 (<a className='link' href='https://maps.app.goo.gl/DM2CvnAiZUbCKW2n8' target='_blank' rel='noreferrer'>Halle Sportinstitut</a>)</p>
-                <h1>Unser Partner</h1>
-                <a href='https://www.naupaka.de/' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/naupaka.png' alt='naupaka' className='partner' style={{width:'20vw'}}/></a>
+                <p><b>Wir sind die Bayreuth Baboons – die Roundnet-Abteilung des USC Bayreuth.</b></p>
+                <p>Vielleicht kennst du das „komische Trampolin-Spiel“ aus dem Park – besser bekannt unter dem Namen Spikeball – und hast es sogar schon selbst ausprobiert. Was du vielleicht noch nicht wusstest: Roundnet ist längst mehr als nur ein Freizeitspiel. In den letzten Jahren hat sich daraus ein rasant wachsender, mittlerweile international organisierter Sport entwickelt – mit Verband, Winterliga (1. Bundesliga, 2. Bundesliga, Regionalliga) und der German Roundnet Tour, einer bundesweiten Turnierserie im Sommer.</p>
+                
+                <p>Bei uns Baboons trainieren wir regelmäßig – mit dem Ziel, spielerisch besser zu werden. In unseren Trainings geht’s nicht nur ums Zocken, sondern auch um gezielte Übungen, mit denen wir wichtige Techniken und Spielverständnis entwickeln.</p>
+                <p><b>Aber: Der Spaß und der Community-Gedanke stehen bei uns immer an erster Stelle.</b></p>
+                <p>Egal ob Anfängerin oder erfahrener Spielerin – bei uns ist jeder willkommen! Wenn du also Lust hast, Roundnet über das Park-Niveau hinaus kennenzulernen, in einer motivierten Gruppe zu trainieren und dich spielerisch weiterzuentwickeln, dann melde dich gerne bei uns.</p>
+                <p><b>Wir freuen uns auf dich – ganz egal auf welchem Level du einsteigst!</b></p>
+
+                <h1>Trainingszeiten 2025</h1>
+                <p style={{ marginTop: 10, fontSize: '14px' }}>ab Mai</p>              
+                
+                <p>Mittwoch 18:00 - Open End (<a className='link' href='https://www.google.de/maps/place/Hofgarten,+95444+Bayreuth/@49.9396724,11.5762312,1196m/data=!3m2!1e3!4b1!4m6!3m5!1s0x47a1a2d163906ce5:0xfe65064915d0116c!8m2!3d49.9396591!4d11.580631!16s%2Fg%2F11bw3gxqpq?entry=ttu&g_ep=EgoyMDI1MDQyMi4wIKXMDSoJLDEwMjExNDUzSAFQAw%3D%3D' target='_blank' rel='noreferrer'>Hofgarten</a>)</p>
+
                 <h1>Team</h1>
                 <Container fluid className='my-container'>
-                    <Row>
-                        <Col className={this.state.mobileMode? 'col-4' : 'col-3'}>
-                            <img src='/img/no-picture-man.jpg' className="contact-picture" alt="staff"/>
-                            <p className='contact-subtitle-title'>Abteilungsleiter</p>
-                            <p className='contact-subtitle'>James Killas</p>
-                            <p className='contact-subtitle'><a className='mail-to contact-subtitle' href='mailto:roundnet.bayreuth@gmail.com'>roundnet.bayreuth@gmail.com</a></p>
-                            <p className='contact-subtitle mobile-number'>0177 4563854</p>
-                        </Col>
-                    </Row>
-                </Container>
+                                    <Row>
+                                        {kader.Verantwortliche.map((item, index) =>{
+                                            return(
+                                                <Col key={index} className={this.state.mobileMode? 'col-4' : 'col-3'}>
+                                                    
+                                                        {item.Bild===undefined &&
+                                                            <img alt={'player'+index} src='/img/no-picture-man.jpg' className='player-picture'/>
+                                                        }
+                                                        {item.Bild!==undefined &&
+                                                            <img alt={'player'+index} src={'/img/Roundnet/'+item.Bild} className='player-picture'/>                                    
+                                                        }
+                                                        {item.Rolle!==undefined &&
+                                                            <p className='contact-subtitle-title'>{item.Rolle}</p>
+                                                        }
+                                                        <p className='contact-subtitle'>{item.Name}</p>
+                                                        {item.Mail!==undefined &&
+                                                            <p className='contact-subtitle'><a className='mail-to contact-subtitle' href={'mailto:'+item.Mail}>{item.Mail}</a></p>
+                                                        }
+                                                        {item.Handy!==undefined &&
+                                                            <p className='mobile-number contact-subtitle'>{item.Handy}</p>
+                                                        }
+                                                   
+                                                </Col>
+                                            )
+                                        })}
+                                    </Row>
+                                </Container>
+
+                <h1>Kontakt</h1>
+                <p>- Instagram: <a href="https://www.instagram.com/bayreuth.baboons" target="_blank" rel="noopener noreferrer">@bayreuth.baboons</a></p>
+                <p>- oder per Mail, an eine Person aus dem Team 😊</p>
+
+                <h1>Unser Partner</h1>
+                <a href='https://www.naupaka.de/' target='_blank' rel='noreferrer'><img src='/img/Sponsoren/naupaka.png' alt='naupaka' className='partner' style={{width:'20vw'}}/></a>
+                
             </>
         )
     }
