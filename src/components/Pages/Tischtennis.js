1a4d16844fc77dda16af8e81b9fce1a08b20ab0c
Erik <reinscherik1@gmail.com>
2026-01-26 21:39:54 +0100

    TT und NEws

--- src/components/Pages/Tischtennis.js
+++ src/components/Pages/Tischtennis.js
@@ -23,11 +23,11 @@ class Tischtennis extends Component{
             <>
                 <h1>Tischtennis</h1>
                <p> Wir freuen uns, die neu gegründete Tischtennisabteilung vorzustellen! Aktuell läuft die Vorbereitung auf unsere erste Wettkampfsaison dort starten wir mit viel Motivation und Teamgeist in der Kreisklasse C – und das ist erst der Anfang.</p>
+                <p>Unser Ziel ist es, in den kommenden Jahren eine lebendige und leistungsstarke Abteilung aufzubauen. Dabei setzen wir auf Nachwuchsarbeit, Teamzusammenhalt und sportliche Entwicklung. Langfristig möchten wir mehrere Mannschaften im Liga- und Pokalbetrieb etablieren.
+                     In der kommenden Saison 26/27 werden wir mit zwei Mannschaften in unterschiedlichen Leistungsklassen an den Start gehen.
+                </p>
                 <p>Unser Ziel ist es, in den kommenden Jahren eine lebendige und leistungsstarke Abteilung aufzubauen. Dabei setzen wir auf Nachwuchsarbeit, Teamzusammenhalt und sportliche Entwicklung. Langfristig möchten wir mehrere Mannschaften im Liga- und Pokalbetrieb etablieren.</p>
-                <p>Du hast Lust, Teil davon zu sein?</p>
-                <p>Egal ob Anfänger, Hobbyspieler oder erfahrener Wettkämpfer – bei uns ist jeder willkommen, der Spaß am Spiel hat und sich in einer offenen, engagierten Gemeinschaft wohlfühlt.</p>
-               <p> Melde dich einfach beim Abteilungsleiter Laurits Kretschmer unter <a className='link' href='mailto:laurits.kretschmer@uni-bayreuth.de'>laurits.kretschmer@uni-bayreuth.de</a></p>
-               
+
                <h1>Team</h1>
                <img alt='Tischtennis' src='/img/Tischtennis/Teamfoto USC TT.jpg' className='mannschaftsfoto'/>
  
